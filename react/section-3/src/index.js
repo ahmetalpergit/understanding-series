27511de2a0bcf0fffb12215ba9c035c0773415aa
Ahmet <ahmetalperwork@gmail.com>
2021-04-08 16:12:14 +0300

    section-3: all props methods / destructuring / props children

--- react/section-3/src/index.js
+++ react/section-3/src/index.js
@@ -3,15 +3,6 @@ import ReactDom from 'react-dom';
 
 import './index.css';
 
-///////////////////////
-// First component
-
-// function Greeting() {
-//   return (
-//     <h4>This is Ahmet and this is my first component.</h4>
-//   )
-// }
-
 ////////////////////////
 // JSX vs JS
 
@@ -31,34 +22,112 @@ import './index.css';
 //   return React.createElement('div', {}, React.createElement('h4', {}, 'hello world'));
 // }
 
-//////////////////////////
-// Nested Components
+/////////////////////////////////
+// PROPS method - 1
+
+// function BookList() {
+//   return (
+//     <section className="booklist">
+//       <Book title="Whatever" author="Someone" img="https://images-na.ssl-images-amazon.com/images/I/51BlTo5rKoL._AC_SX184_.jpg" />
+//       <Book title='The 48 Laws of Power' author = 'harry potter' img = 'https://images-na.ssl-images-amazon.com/images/I/51ucj27xFCL._AC_SX184_.jpg'/>
+//     </section>
+//   )
+// }
+
+// const Book = (props) => {
+  
+//   return (
+//     <article className="book" style={{padding: "1rem", backgroundColor: "#fff", borderRadius: "10px"}}>
+//       <img src={props.img} alt=""/>
+//       <h1>{props.title}</h1>
+//       <p>{props.author}</p>
+//     </article>
+//   )
+// }
+
+/////////////////////////////////
+// PROPS method - 2
+
+// function BookList() {
+//   return (
+//     <section className="booklist">
+//       <Book title="Whatever" author="Someone" img="https://images-na.ssl-images-amazon.com/images/I/51BlTo5rKoL._AC_SX184_.jpg" />
+//       <Book title='The 48 Laws of Power' author = 'harry potter' img = 'https://images-na.ssl-images-amazon.com/images/I/51ucj27xFCL._AC_SX184_.jpg'/>
+//     </section>
+//   )
+// }
+
+// const Book = (props) => {
+
+//   const { title, author, img } = props;
+  
+//   return (
+//     <article className="book" style={{padding: "1rem", backgroundColor: "#fff", borderRadius: "10px"}}>
+//       <img src={img} alt=""/>
+//       <h1>{title}</h1>
+//       <p>{author}</p>
+//     </article>
+//   )
+// }
+
+/////////////////////////////////
+// PROPS method - 3
+
+// function BookList() {
+//   return (
+//     <section className="booklist">
+//       <Book title="Whatever" author="Someone" img="https://images-na.ssl-images-amazon.com/images/I/51BlTo5rKoL._AC_SX184_.jpg" />
+//       <Book title='The 48 Laws of Power' author = 'harry potter' img = 'https://images-na.ssl-images-amazon.com/images/I/51ucj27xFCL._AC_SX184_.jpg'/>
+//     </section>
+//   )
+// }
+
+// const Book = ({
+//   title,
+//   author,
+//   img
+// }) => {
+
+//   return (
+//     <article className="book" style={{padding: "1rem", backgroundColor: "#fff", borderRadius: "10px"}}>
+//       <img src={img} alt=""/>
+//       <h1>{title}</h1>
+//       <p>{author}</p>
+//     </article>
+//   )
+// }
+
+/////////////////////////////////
+// PROPS children
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book title="Whatever" author="Someone" img="https://images-na.ssl-images-amazon.com/images/I/51BlTo5rKoL._AC_SX184_.jpg">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum quidem iusto ullam asperiores sequi vel, cum, sapiente laboriosam, libero deleniti iure reiciendis pariatur iste nisi. Excepturi labore sit temporibus culpa?</p>
+      </Book>
+      <Book title='The 48 Laws of Power' author = 'harry potter' img = 'https://images-na.ssl-images-amazon.com/images/I/51ucj27xFCL._AC_SX184_.jpg'/>
     </section>
   )
 }
 
-const Book = () => {
-  const title = 'The 48 Laws of Power';
-  const author = 'harry potter';
-  
+const Book = ({
+  title,
+  author,
+  img,
+  children //special name that denotes children, always.
+}) => {
+
   return (
     <article className="book" style={{padding: "1rem", backgroundColor: "#fff", borderRadius: "10px"}}>
-      <img src="https://images-na.ssl-images-amazon.com/images/I/41JIh4KMHRL._AC_SX184_.jpg" alt=""/>
+      <img src={img} alt=""/>
       <h1>{title}</h1>
-      <p>{author.toUpperCase()}</p>
-      <p>{6 + 6}</p>
+      <p>{author}</p>
+      {children}
     </article>
   )
 }
+
+
 
 ReactDom.render(<BookList />, document.getElementById('root'));
