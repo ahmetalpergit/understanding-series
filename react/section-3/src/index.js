445098c0e8d91318dcf60766986876c9a0a9dd01
Ahmet <ahmetalperwork@gmail.com>
2021-04-08 11:49:03 +0300

    section-3: why we use html returns and jsx instead of React.createElement

--- react/section-3/src/index.js
+++ react/section-3/src/index.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDom from 'react-dom';
 
-function Greeting() {
-  return (
-    <h4>This is Ahmet and this is my first component.</h4>
-  )
+// function Greeting() {
+//   return (
+//     <h4>This is Ahmet and this is my first component.</h4>
+//   )
+// }
+
+// const Greeting = () => {
+//   return React.createElement('h4', {}, 'hello world');
+// }
+
+// const Greeting = () => {
+//   return (
+//     <div>
+//       <h4>This is Ahmet and this is my first component.</h4>   
+//     </div>
+//   )
+// }
+
+const Greeting = () => {
+  return React.createElement('div', {}, React.createElement('h4', {}, 'hello world'));
 }
 
 ReactDom.render(<Greeting />, document.getElementById('root'));
