48037ec7f10560c90512717b07007754afbcea7e
Ahmet <ahmetalperwork@gmail.com>
2021-04-08 12:08:16 +0300

    section-3: nested components

--- react/section-3/src/index.js
+++ react/section-3/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDom from 'react-dom';
 
+///////////////////////
+// First component
+
 // function Greeting() {
 //   return (
 //     <h4>This is Ahmet and this is my first component.</h4>
 //   )
 // }
+
+////////////////////////
+// JSX vs JS
 
 // const Greeting = () => {
 //   return React.createElement('h4', {}, 'hello world');
@@ -19,8 +25,23 @@ import ReactDom from 'react-dom';
 //   )
 // }
 
-const Greeting = () => {
-  return React.createElement('div', {}, React.createElement('h4', {}, 'hello world'));
+// const Greeting = () => {
+//   return React.createElement('div', {}, React.createElement('h4', {}, 'hello world'));
+// }
+
+//////////////////////////
+// Nested Components
+
+function Greeting() {
+  return (
+    <div>
+      <Person />
+      <Message />
+    </div>
+  )
 }
+
+const Person = () => <h1>John Doe</h1>
+const Message = () => <p>This is my message.</p>
 
 ReactDom.render(<Greeting />, document.getElementById('root'));
