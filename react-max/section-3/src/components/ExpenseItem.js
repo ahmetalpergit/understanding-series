7c6b8006ed72313251778e989c0f1a545a0c724a
Ahmet <ahmetalperwork@gmail.com>
2021-04-13 13:21:52 +0300

    adding styling to react components

--- react-max/section-3/src/components/ExpenseItem.js
+++ react-max/section-3/src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
-    return <h2>This is an expense!</h2>
+    return (
+        <div className="expense-item">
+            <div>April 13th 2021</div>
+            <div className="expense-item__description">
+                <h2>Car Insurance</h2>
+                <div className="expense-item__price">$294.67</div>
+            </div>
+        </div>
+    )
 }
 
 export default ExpenseItem
