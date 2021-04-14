c796e7b12065ee98fb499897010d2c6f8b9ec03f
Ahmet <ahmetalperwork@gmail.com>
2021-04-14 08:41:27 +0300

    splitting components and drilling props to children

--- react-max/section-3/src/components/ExpenseItem.js
+++ react-max/section-3/src/components/ExpenseItem.js
@@ -1,15 +1,13 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem({
     title, amount, date
 }) {
-    // const expenseDate = new Date(2021, 3, 12);
-    // const expenseTitle = 'Car Insurance';
-    // const expensePrice = 294.67;
 
     return (
         <div className="expense-item">
-            <div>{date.toLocaleDateString()}</div>
+            <ExpenseDate date={date} />
             <div className="expense-item__description">
                 <h2>{title}</h2>
                 <div className="expense-item__price">${amount}</div>
