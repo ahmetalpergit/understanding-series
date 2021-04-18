0f46d876f8c15e4d0f30cf01b4e227d0a7754f9b
Ahmet <ahmetalperwork@gmail.com>
2021-04-18 12:41:03 +0300

    two-way binding for form values

--- react-max/section-4/src/components/NewExpense/ExpenseForm.js
+++ react-max/section-4/src/components/NewExpense/ExpenseForm.js
@@ -4,50 +4,60 @@ import './ExpenseForm.css';
 
 function ExpenseForm() {
 
-    //const [currentTitle, setCurrentTitle] = useState('');
-    //const [currentAmount, setCurrentAmount] = useState('');
-    //const [currentDate, setCurrentDate] = useState('');
-    const [userInput, setUserInput] = useState({
-        currentTitle: '',
-        currentAmount: '',
-        currentDate: ''
-    });
+    const [currentTitle, setCurrentTitle] = useState('');
+    const [currentAmount, setCurrentAmount] = useState('');
+    const [currentDate, setCurrentDate] = useState('');
+    //const [userInput, setUserInput] = useState({
+    //    currentTitle: '',
+    //    currentAmount: '',
+    //    currentDate: ''
+    //});
 
 
     const titleChangeHandler = (e) => {
-        //setCurrentTitle(e.target.value);
+        setCurrentTitle(e.target.value);
         //setUserInput({
         //    ...userInput,
         //    currentTitle: e.target.value
         //});
 
-        setUserInput(prev => {
-            return {
-                ...userInput,
-                currentTitle: e.target.value
-            };
-        });
+        //setUserInput(prev => {
+        //    return {
+        //        ...userInput,
+        //        currentTitle: e.target.value
+        //    };
+        //});
     };
 
     const amountChangeHandler = (e) => {
-        //setCurrentAmount(e.target.value);
-        setUserInput({
-            ...userInput,
-            currentAmount: e.target.value
-        });
+        setCurrentAmount(e.target.value);
+        //setUserInput({
+        //    ...userInput,
+        //    currentAmount: e.target.value
+        //});
     };
 
     const dateChangeHandler = (e) => {
-        //setCurrentDate(e.target.value);
-        setUserInput({
-            ...userInput,
-            currentDate: e.target.value
-        });
+        setCurrentDate(e.target.value);
+        //setUserInput({
+        //    ...userInput,
+        //    currentDate: e.target.value
+        //});
     };
 
     const logDataHandler = (e) => {
         e.preventDefault();
-        console.log(userInput);
+
+        const expenseData = {
+            title: currentTitle,
+            amount: currentAmount,
+            date: new Date(currentDate),
+        };
+
+        setCurrentTitle('');
+        setCurrentAmount('');
+        setCurrentDate('');
+        console.log(expenseData);
     };
 
     return (
@@ -55,15 +65,15 @@ function ExpenseForm() {
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label htmlFor="">Title</label>
-                    <input type="text" onChange={titleChangeHandler} />
+                    <input type="text" value={currentTitle} onChange={titleChangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label htmlFor="">Amount</label>
-                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+                    <input type="number" value={currentAmount} min="0.01" step="0.01" onChange={amountChangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label htmlFor="">Date</label>
-                    <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
+                    <input type="date" value={currentDate} min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
                 </div>
             </div>
             <div className="new-expense__actions">
