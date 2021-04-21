82800857d4b72c371a477067c7ce4d46f27a8477
Ahmet <ahmetalperwork@gmail.com>
2021-04-21 19:03:58 +0300

    setting inline-styles dynamically

--- react-max/section-6/src/components/CourseGoals/CourseInput/CourseInput.js
+++ react-max/section-6/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,25 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValidInput, setIsValidInput] = useState(true);
+
 
   const goalInputChangeHandler = event => {
+    if (enteredValue.trim().length > 0) setIsValidInput(true);
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) return setIsValidInput(false);
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{ color: isValidInput ? 'black' : 'red' }}>Course Goal</label>
+        <input type="text" style={{ border: !isValidInput && '1px solid red', backgroundColor: !isValidInput && 'pink' }} onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
