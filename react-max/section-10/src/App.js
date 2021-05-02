378fc711aba52764c7b966ec8845223343b7f3f7
Ahmet <ahmetalperwork@gmail.com>
2021-05-02 11:32:19 +0300

    passing state through Context without hooks using Consumer

--- react-max/section-10/src/App.js
+++ react-max/section-10/src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import ContextAuth from './components/store/context-auth';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -27,13 +28,13 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <ContextAuth.Provider value={{ loginValidation: isLoggedIn }}>
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </ContextAuth.Provider>
   );
 }
 
--- react-max/section-10/src/components/MainHeader/Navigation.js
+++ react-max/section-10/src/components/MainHeader/Navigation.js
@@ -1,28 +1,35 @@
 import React from 'react';
+import ContextAuth from '../store/context-auth';
 
 import classes from './Navigation.module.css';
 
 const Navigation = (props) => {
   return (
-    <nav className={classes.nav}>
-      <ul>
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Users</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Admin</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <button onClick={props.onLogout}>Logout</button>
-          </li>
-        )}
-      </ul>
-    </nav>
+    <ContextAuth.Consumer>
+      {(context) => {
+        return (
+          <nav className={classes.nav}>
+            <ul>
+              {context.loginValidation && (
+                <li>
+                  <a href="/">Users</a>
+                </li>
+              )}
+              {context.loginValidation && (
+                <li>
+                  <a href="/">Admin</a>
+                </li>
+              )}
+              {context.loginValidation && (
+                <li>
+                  <button onClick={props.onLogout}>Logout</button>
+                </li>
+              )}
+            </ul>
+          </nav>
+        );
+      }}
+    </ContextAuth.Consumer>
   );
 };
 
