41dbaac09694dd16c2774470ea33719307134e9e
Ahmet <ahmetalperwork@gmail.com>
2021-05-02 11:37:21 +0300

    context using useContext hook (so much better)

--- react-max/section-10/src/components/MainHeader/Navigation.js
+++ react-max/section-10/src/components/MainHeader/Navigation.js
@@ -1,35 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ContextAuth from '../store/context-auth';
 
 import classes from './Navigation.module.css';
 
 const Navigation = (props) => {
+
+  const context = useContext(ContextAuth);
+
   return (
-    <ContextAuth.Consumer>
-      {(context) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {context.loginValidation && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {context.loginValidation && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {context.loginValidation && (
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
-      }}
-    </ContextAuth.Consumer>
+    <nav className={classes.nav}>
+      <ul>
+        {context.loginValidation && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {context.loginValidation && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {context.loginValidation && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
