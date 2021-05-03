365f20908c42d838e3ef10b6f4c7f9ea8035f173
Ahmet <ahmetalperwork@gmail.com>
2021-05-03 21:44:34 +0300

    add HeaderCartButton and CartIcon components

--- react-max/section-11/src/components/Layout/Header.js
+++ react-max/section-11/src/components/Layout/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import HeaderCartButton from './HeaderCartButton';
 import classes from './Header.module.css';
 import mealsImage from '../../assets/meals.jpg';
 
@@ -7,7 +8,7 @@ const Header = () => {
         <>
             <header className={classes.header}>
                 <h1>React Meals</h1>
-                <button>Cart</button>
+                <HeaderCartButton />
             </header>
             <div className={classes['main-image']}>
                 <img src={mealsImage} alt="a table full of delicious food" />
