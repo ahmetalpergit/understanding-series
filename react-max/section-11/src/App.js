d5197facedf0eb33717f7c5d0694f201267a88b5
Ahmet <ahmetalperwork@gmail.com>
2021-05-03 22:14:46 +0300

    create MealsSummary component and render Dummy data

--- react-max/section-11/src/App.js
+++ react-max/section-11/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from './components/Layout/Header';
+import AvailableMeals from './components/Meals/AvailableMeals';
+import MealsSummary from './components/Meals/MealsSummary';
 
 function App() {
   return (
     <>
       <Header />
+      <MealsSummary />
+      <AvailableMeals />
     </>
   );
 }
