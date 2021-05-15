5e5f619fbe766df98a9cd91ce86ae9aa4ec4d699
Ahmet <ahmetalperwork@gmail.com>
2021-05-15 23:33:29 +0200

    using the useEffect hook

--- react-max/section-11/src/components/Layout/HeaderCartButton.js
+++ react-max/section-11/src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,25 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 import CartContext from '../../store/context-cart';
 
 const HeaderCartButton = ({ onClick }) => {
-
+    const [btnBump, setBtnBump] = useState(false);
     const ctxCart = useContext(CartContext);
     const numberOfCartItems = ctxCart.items.reduce((accTotal, curItem) => accTotal + curItem.amount, 0);
 
+    useEffect(() => {
+        if (ctxCart.items.length === 0) return;
+        setBtnBump(true);
+        const timer = setTimeout(() => {
+            setBtnBump(false);
+        }, 300);
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [ctxCart.items]);
     return (
-        <button className={classes.button} onClick={onClick}>
+        <button className={btnBump ? `${classes.button} ${classes.bump}` : classes.button} onClick={onClick}>
             <span className={classes.icon}><CartIcon /></span>
             <span>Your Cart</span>
             <span className={classes.badge}>{numberOfCartItems}</span>
