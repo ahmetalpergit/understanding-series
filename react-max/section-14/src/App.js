c44cc7db2d2f082d11cce2ea6aed43de5e9de868
Ahmet <ahmetalperwork@gmail.com>
2021-05-21 15:51:02 +0200

    Using async / await

--- react-max/section-14/src/App.js
+++ react-max/section-14/src/App.js
@@ -7,21 +7,35 @@ function App() {
 
   const [movies, setMovies] = useState([]);
 
-  const fetchMoviesHandler = () => {
-    fetch('http://swapi.dev/api/films/').then(res => {
-      return res.json();
-    }).then(data => {
-      const { results } = data;
-      const formattedData = results.map(data => {
-        return {
-          id: data.episode_id,
-          title: data.title,
-          releaseData: data.release_date,
-          openingText: data.opening_crawl
-        };
-      });
-      setMovies(formattedData);
+  const fetchMoviesHandler = async () => {
+    //fetch('http://swapi.dev/api/films/').then(res => {
+    //  return res.json();
+    //}).then(data => {
+    //  const { results } = data;
+    //  const formattedData = results.map(data => {
+    //    return {
+    //      id: data.episode_id,
+    //      title: data.title,
+    //      releaseData: data.release_date,
+    //      openingText: data.opening_crawl
+    //    };
+    //  });
+    //  setMovies(formattedData);
+    //});
+
+    const res = await fetch('http://swapi.dev/api/films/');
+    const data = await res.json();
+
+    const { results } = data;
+    const formattedData = results.map(data => {
+      return {
+        id: data.episode_id,
+        title: data.title,
+        releaseData: data.release_date,
+        openingText: data.opening_crawl
+      };
     });
+    setMovies(formattedData);
   };
 
   return (
