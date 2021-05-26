115e3084d04ce15378dd1c712da36f9b05bc97e9
Ahmet <ahmetalperwork@gmail.com>
2021-05-26 20:57:34 +0200

    using the custom hook in more components

--- react-max/section-15/src/components/NewTask/NewTask.js
+++ react-max/section-15/src/components/NewTask/NewTask.js
@@ -1,41 +1,28 @@
-import { useState } from 'react';
-
+import { useHttp } from '../../hooks/useHttp';
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+
+  const { isLoading, error, sendRequest: createTask } = useHttp();
+
+  const processData = (taskText, data) => {
+    const generatedId = data.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-max-http-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
+    createTask({
+      url: 'https://react-max-http-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: { text: taskText },
+    }, processData.bind(null, taskText));
   };
 
   return (
