b9831c33f0b45dc8ba4376f8c54c82fd0612226f
Ahmet <ahmetalperwork@gmail.com>
2021-05-27 12:53:50 +0200

    dealing with form submission & getting user input values

--- react-max/section-16/src/components/SimpleInput.js
+++ react-max/section-16/src/components/SimpleInput.js
@@ -1,9 +1,28 @@
+import { useState, useRef } from 'react';
+
 const SimpleInput = (props) => {
+
+  const [input, setInput] = useState('');
+  const nameInputRef = useRef();
+
+  const submitFormHandler = (e) => {
+    e.preventDefault();
+    const enteredName = nameInputRef.current.value;
+    console.log(input);
+    console.log(enteredName);
+    setInput('');
+    //nameInputRef.current.value = ''; //Not IDEAL - DO NOT manipulate DOM directly
+  };
+
+  const inputChangeHandler = (e) => {
+    setInput(e.target.value);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitFormHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' onChange={inputChangeHandler} value={input} ref={nameInputRef} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
