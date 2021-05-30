4a55fd46af87491b0aed5246952f3ebbd42cd1a5
Ahmet <ahmetalperwork@gmail.com>
2021-05-30 15:32:44 +0200

    handling the wastouched State

--- react-max/section-16/src/components/SimpleInput.js
+++ react-max/section-16/src/components/SimpleInput.js
@@ -1,14 +1,20 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 const SimpleInput = (props) => {
 
-  const [input, setInput] = useState('');
-  const [inputIsValid, setInputIsValid] = useState(true);
   const nameInputRef = useRef();
+  const [input, setInput] = useState('');
+  const [inputIsValid, setInputIsValid] = useState(false);
+  const [inputIsTouched, setInputIsTouched] = useState(false);
+
+  useEffect(() => {
+    if (inputIsValid) console.log('valid input');
+  }, [inputIsValid]);
 
   const submitFormHandler = (e) => {
     e.preventDefault();
 
+    setInputIsTouched(true);
     if (input.trim() === '') return setInputIsValid(false);
 
     setInputIsValid(true);
@@ -22,10 +28,10 @@ const SimpleInput = (props) => {
 
   return (
     <form onSubmit={submitFormHandler}>
-      <div className={`form-control ${!inputIsValid ? 'invalid' : ''}`}>
+      <div className={`form-control ${!inputIsValid && inputIsTouched ? 'invalid' : ''}`}>
         <label htmlFor='name'>Your Name</label>
         <input type='text' id='name' onChange={inputChangeHandler} value={input} ref={nameInputRef} />
-        {!inputIsValid && <p className="error-text">Name must not be empty.</p>}
+        {!inputIsValid && inputIsTouched && <p className="error-text">Name must not be empty.</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
