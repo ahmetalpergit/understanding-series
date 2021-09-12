8590d40d36d71557262001a867f3620a6b770fc5
Ahmet <ahmetalperwork@gmail.com>
2021-09-12 20:48:21 +0200

    2-14: finish lecture

--- typescript/app.js
+++ typescript/app.js
@@ -1,7 +1,13 @@
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, result, phrase) {
+    if (result) {
+        console.log(phrase + (n1 + n2));
+    }
+    else {
+        return n1 + n2;
+    }
 }
 var number1 = 5;
 var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+var showResult = true;
+var showPhrase = "The result is: ";
+add(number1, number2, showResult, showPhrase);
