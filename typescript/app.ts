a67fcd8b26c591417e10b87a1dc7d31dd4c322e3
Ahmet <ahmetalperwork@gmail.com>
2021-09-13 21:23:13 +0200

    lecture 2-21: finished

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,57 +1,12 @@
-//Bad Example
+//any[] allows any sort of data inside an array
 
-//const person: { name: string; age: number } = {
-//  name: "Maximilian",
-//  age: 30,
-//};
+let favoriteActivities: any[];
+favoriteActivities = ['Sports', 5];
 
-//Good Example
+//it checks for array though
 
-//const person: {
-//    name: string;
-//    age: number;
-//    hobbies: string[];
-//    role: [number, string];
-//} = {
-//    name: 'Maximilian',
-//    age: 30,
-//    hobbies: ['Sports', 'Cooking'],
-//    role: [2, 'author'],
-//};
+let anyCheckArray: any[];
+//anyCheckArray = 5;
 
-//TS cannot catch pushing for tuples - be careful!
-//person.role.push(5);
-
-//TS catches these
-//person.role[1] = 5;
-//person.role[2] = 10;
-
-let favoriteActivities: string[];
-favoriteActivities = ['Sports'];
-
-//console.log(person.name);
-
-//for (const hobby of person.hobbies) {
-//    console.log(hobby.toUpperCase());
-//}
-
-//const ADMIN = 0;
-//const READ_ONLY = 1;
-//const AUTHOR = 2;
-
-enum Role {
-    ADMIN = 'ADMIN',
-    READ_ONLY = 1,
-    AUTHOR = 'AUTHOR',
-}
-
-const person = {
-    name: 'Maximilian',
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: Role.ADMIN,
-};
-
-if (person.role === Role.ADMIN) {
-    console.log('is admin');
-}
+let someValue: any = 5;
+let someOtherValue: any = 'here I am';
