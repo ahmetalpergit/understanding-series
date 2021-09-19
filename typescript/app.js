b2fc6171de311cecf791e6c6c6e0485b49a6338d
Ahmet <ahmetalperwork@gmail.com>
2021-09-19 14:06:45 +0200

    lecture 2-27: functions as types

--- typescript/app.js
+++ typescript/app.js
@@ -1,7 +1,14 @@
 function add(n1, n2) {
     return n1 + n2;
 }
+//void return type - doesn't return anything
 function printResult(num) {
     console.log('The result is: ' + num);
 }
-printResult(add(5, 10));
+// 1- perfectly fine pointer function
+var combineValues = add;
+console.log(combineValues(5, 10));
+// 2- This is how we define a function type (param names don't matter)
+var combineValues2 = add;
+// 3- Throws error because the function we're pointing doesn't satisfy our function type
+//let combineValues3: (a: number, b: number) => number = printResult;
--- typescript/app.ts
+++ typescript/app.ts
@@ -7,12 +7,12 @@ function printResult(num: number): void {
     console.log('The result is: ' + num);
 }
 
-//can be undefined type without error since it has return statement
-function voidResult(num: number): undefined {
-    console.log('The result is: ' + num);
-    return;
-}
+// 1- perfectly fine pointer function
+let combineValues = add;
+console.log(combineValues(5, 10));
 
-printResult(add(5, 10));
+// 2- This is how we define a function type (param names don't matter)
+let combineValues2: (a: number, b: number) => number = add;
 
-//Best is to let functions infer the return types unless you specifially want a definite return type.
+// 3- Throws error because the function we're pointing doesn't satisfy our function type
+//let combineValues3: (a: number, b: number) => number = printResult;
