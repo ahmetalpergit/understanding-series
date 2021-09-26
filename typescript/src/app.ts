0812e4fa721510253ceea3f74e97201f3ae8cb81
Ahmet <ahmetalperwork@gmail.com>
2021-09-26 22:33:46 +0200

    constructor functions & the this keyword

--- typescript/src/app.ts
+++ typescript/src/app.ts
@@ -4,8 +4,20 @@ class Department {
     constructor(n: string) {
         this.name = n;
     }
+
+    //adds type check for the object that uses the method to have the same structure as Department
+    describe(this: Department) {
+        console.log('Department: ' + this.name);
+    }
 }
 
 const accounting = new Department('Accounting');
 
-console.log(accounting);
+accounting.describe();
+
+//Throws error unless accountingCopy gets a name property
+//const accountingCopy = { describe: accounting.describe };
+
+const accountingCopy = { name: 'Math', describe: accounting.describe };
+
+accountingCopy.describe();
