a46857311589aa45a26deb88c88ed03ac5897a96
Ahmet <ahmetalperwork@gmail.com>
2021-09-26 22:46:20 +0200

    shorthand initialization

--- typescript/src/app.ts
+++ typescript/src/app.ts
@@ -1,37 +1,17 @@
 class Department {
-    name: string;
-    private employees: string[] = [];
+    //private name: string;
+    //private id: string;
 
-    constructor(n: string) {
-        this.name = n;
+    //shorthand to create class with variables right away
+    constructor(private id: string, private name: string) {
+        //this.name = n;
     }
 
-    //adds type check for the object that uses the method to have the same structure as Department
     describe(this: Department) {
-        console.log('Department: ' + this.name);
-    }
-
-    addEmployee(employee: string) {
-        this.employees.push(employee);
-    }
-
-    checkEmployeeInfo() {
-        console.log(this.employees.length);
-        console.log(this.employees);
+        console.log(`Department: ${this.id}: ${this.name}`);
     }
 }
 
-const accounting = new Department('Accounting');
-
-accounting.addEmployee('Max');
-accounting.addEmployee('Manu');
-
-accounting.checkEmployeeInfo();
-
-//due to private variable, it will throw error
-//accounting.employees[2] = 'Ahmet'
-
-//public variable, therefore still accessible
-accounting.name = 'NEW NAME';
+const accounting = new Department('d1', 'Accounting');
 
 accounting.describe();
