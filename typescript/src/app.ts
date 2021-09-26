6dc057fc148e7b75b5e49d59528e45b0b62336c5
Ahmet <ahmetalperwork@gmail.com>
2021-09-26 23:13:50 +0200

    inheritance

--- typescript/src/app.ts
+++ typescript/src/app.ts
@@ -1,13 +1,33 @@
 class Department {
-    constructor(private readonly id: string, private name: string) {}
+    constructor(
+        private readonly id: string,
+        private name: string,
+        private employee: string[] = []
+    ) {}
 
     describe(this: Department) {
-        //not allowed because id is read-only
-        //this.id = 'd2'
         console.log(`Department: ${this.id}: ${this.name}`);
+    }
+
+    addEmployee(employee: string[]) {
+        this.employee.push(...employee);
     }
 }
 
 const accounting = new Department('d1', 'Accounting');
 
 accounting.describe();
+
+class ITDepartment extends Department {
+    public admins: string[];
+    constructor(id: string, admins: string[]) {
+        super(id, 'IT');
+        this.admins = admins;
+    }
+}
+
+const it = new ITDepartment('d2', ['Max']);
+it.addEmployee(['Anna', 'George']);
+console.log(it);
+
+it.describe();
