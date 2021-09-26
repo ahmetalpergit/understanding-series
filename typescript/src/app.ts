ebdba977bf781a7f4547ffe24f912d2214ac46a6
Ahmet <ahmetalperwork@gmail.com>
2021-09-26 22:21:08 +0200

    creating first class

--- typescript/src/app.ts
+++ typescript/src/app.ts
@@ -1,5 +1,11 @@
-const button = document.querySelector('button')!;
+class Department {
+    name: string;
 
-button.addEventListener('click', () => {
-    console.log('clicked');
-});
+    constructor(n: string) {
+        this.name = n;
+    }
+}
+
+const accounting = new Department('Accounting');
+
+console.log(accounting);
