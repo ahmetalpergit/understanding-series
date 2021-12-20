40190d0eb64b65eed67a7ced33b58e2886e0978f
Ahmet <ahmetalperwork@gmail.com>
2021-12-20 05:32:09 +0100

    typescript-9-123: interaction with dom elements

--- typescript/src/app.ts
+++ typescript/src/app.ts
@@ -3,17 +3,36 @@ class ProjectInput {
    templateElement: HTMLTemplateElement;
    hostElement: HTMLDivElement;
    element: HTMLFormElement;
+   titleInputElement: HTMLInputElement;
+   descriptionInputElement: HTMLInputElement;
+   peopleInputElement: HTMLInputElement;
+
    constructor() {
       this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
       this.hostElement = document.getElementById('app')! as HTMLDivElement;
 
       const importedNode = document.importNode(this.templateElement.content, true);
       this.element = importedNode.firstElementChild as HTMLFormElement;
+      this.element.id = 'user-input';
+
+      this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+      this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+      this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+      this.configure();
       this.attach(this.element);
    }
 
    private attach(attachment: HTMLElement) {
       this.hostElement.insertAdjacentElement('afterbegin', attachment);
+   }
+
+   private submitHandler(e: Event) {
+      e.preventDefault();
+   }
+
+   private configure() {
+      this.element.addEventListener('submit', this.submitHandler.bind(this));
    }
 }
 
