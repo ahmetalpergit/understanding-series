470141c1caa2a98a56f633fa5aa22dd6d5213d1c
Ahmet <ahmetalperwork@gmail.com>
2021-12-25 19:39:41 +0100

    john section 4 until 4-45

---
+++ react-john/react-hooks-advanced-topics/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CounterExample from './tutorial/1-useState/setup/5-useState-counter';
+
+function App() {
+  return (
+    <div className='container'>
+      <CounterExample />
+    </div>
+  );
+}
+
+export default App;
