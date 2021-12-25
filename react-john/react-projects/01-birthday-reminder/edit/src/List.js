1c8bb32a4c7ce4d4b4dd6782387025407b2ad4f4
Ahmet <ahmetalperwork@gmail.com>
2021-12-25 20:33:47 +0100

    final commit

--- react-john/react-projects/01-birthday-reminder/edit/src/List.js
+++ react-john/react-projects/01-birthday-reminder/edit/src/List.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Person from './Person';
 
-const List = () => {
+const List = ({ data }) => {
+  const [people, setPeople] = useState(data);
+
+  const clickHandler = () => {
+    setPeople([]);
+  };
+
   return (
-    <>
-      <h2>list component</h2>
-    </>
+    <section className='container'>
+      <h3>{people.length} birthdays today</h3>
+      {people.map(el => <Person key={el.id} {...el} />)}
+      <button onClick={clickHandler}>Clear all</button>
+    </section>
   );
 };
 
