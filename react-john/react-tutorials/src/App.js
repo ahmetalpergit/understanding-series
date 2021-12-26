ab4459b476ccd7eae294a971865efe126e785020
Ahmet <ahmetalperwork@gmail.com>
2021-12-26 22:36:20 +0100

    tutorials-4-50: useEffect - cleanup function

---
+++ react-john/react-tutorials/src/App.js
@@ -0,0 +1,12 @@
+import UseEffectCleanup from '../src/tutorial/2-useEffect/setup/2-useEffect-cleanup';
+
+import React from 'react';
+function App() {
+  return (
+    <div className='container'>
+      <UseEffectCleanup />
+    </div>
+  );
+}
+
+export default App;
